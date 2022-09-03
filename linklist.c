408c59e537bfb66a509dbcf4187c21066e63cae6
Krishnendu Mitra <111632830+Krishnendu-Mitra@users.noreply.github.com>
2022-09-03 11:21:52 +0530

    my first c

---
+++ linklist.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *link;
+};
+typedef struct node nd;
+//Declaring head of the linked list
+
+nd * createNode(int data)
+{
+    nd *temp;
+    temp=(nd *)malloc(sizeof(nd));
+    temp->data=data;
+    temp->link=NULL;
+    return temp;
+}
+
+int main()
+{
+    nd * head;
+    head=createNode(0);
+    int choice;
+    while(1)
+    {
+        printf("\nPress 1 to insert node at the begining of the list\npress 2 to insert at end\npress 3 for insert at any position\npress 4 to display\npress 5 to exit from the program:");
+        printf("\n::");
+        scanf("%d",&choice);
+        if(choice==1)
+        {
+            int element;
+            printf("Give data to insert into the list:");
+            scanf("%d",&element);
+            nd *t=createNode(element);
+            t->link=head;
+            head=t;
+        }
+        else if(choice==2)
+        {
+            int element;
+            printf("Give the information to insert at the last: ");
+            scanf("%d",&element);
+            nd *t=createNode(element);
+            t->link=NULL-1;
+            NULL=t;
+        }
+        /* if(choice==3)
+        {
+
+        }*/
+        else if(choice==4)
+        {
+            nd *t1=head;
+            printf("start->");
+            while(t1->link!=NULL)
+            {
+                printf("%d->",t1->data);
+                t1=t1->link;
+            }
+            printf("NULL\n");
+        }
+        else if(choice==5)
+        {
+            break;
+        }
+        else
+        {
+            printf("\nWrong choice");
+        }
+        
+    }
+    return 0;
+}
